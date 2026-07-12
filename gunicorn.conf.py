7bd1e1b7b676f3e9fd88abfe1344e3ee8f739ceb
Byungyeon Ko <72363124+ZuriKo83@users.noreply.github.com>
2026-07-12 11:22:46 +0900

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 bind = '0.0.0.0:8021'
-workers = max(2, multiprocessing.cpu_count() * 2 + 1)
+workers = 2
 worker_class = 'uvicorn.workers.UvicornWorker'
 timeout = 60
 graceful_timeout = 30
