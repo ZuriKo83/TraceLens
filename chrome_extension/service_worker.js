a4f7d4f0e2eb74299d38b205d9a43e4314e4c72e
Byungyeon Ko <72363124+ZuriKo83@users.noreply.github.com>
2026-07-14 15:12:00 +0900

    Load persistent scan channel

--- chrome_extension/service_worker.js
+++ chrome_extension/service_worker.js
@@ -1,1 +1,1 @@
-importScripts("background.js", "youtube_activity_collector.js", "youtube_live_chat_collector_v2.js", "delete_worker_v2.js");
+importScripts("background.js", "youtube_activity_collector.js", "youtube_live_chat_collector_v2.js", "scan_channel_v2.js", "delete_worker_v2.js");
