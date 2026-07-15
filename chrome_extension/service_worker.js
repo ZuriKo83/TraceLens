350da8fea19ee92610741e018f35a3dfb1487f23
Byungyeon Ko <72363124+ZuriKo83@users.noreply.github.com>
2026-07-16 05:20:39 +0900

    feat: load YouTube live chat deletion handlers

--- chrome_extension/service_worker.js
+++ chrome_extension/service_worker.js
@@ -4,5 +4,7 @@ importScripts(
   "deletion_engine.js",
   "youtube_delete_page.js",
   "youtube_verify_page.js",
+  "youtube_live_chat_delete_page.js",
+  "youtube_live_chat_verify_page.js",
   "youtube_deletion_adapter.js"
 );
