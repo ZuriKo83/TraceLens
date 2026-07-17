4f1ca11ae40ea90ad9cdfd9ab77d8feecffb4650
Byungyeon Ko <72363124+ZuriKo83@users.noreply.github.com>
2026-07-17 16:43:02 +0900

    refactor: simplify extension service worker imports

--- chrome_extension/service_worker.js
+++ chrome_extension/service_worker.js
@@ -1,8 +1,7 @@
 importScripts(
   "background.js",
+  "youtube_delete_page.js",
   "youtube_activity_collector.js",
   "deletion_engine_resilient.js",
-  "youtube_delete_page.js",
-  "youtube_activity_verify_page.js",
   "youtube_deletion_adapter.js"
 );
